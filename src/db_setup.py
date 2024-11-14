eef88b8580d9ccaf830fc15e2a71e5c810971605
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2024-11-14 15:52:54 +0200

    added db_setup.py to initialize database

---
+++ src/db_setup.py
@@ -0,0 +1,25 @@
+from config import db, app
+from sqlalchemy import text
+
+table = "reference_list"
+
+def initialize_database():
+    print(f"Checking if table {table} exists")
+    sql = text("SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name= :table)")
+    result = db.session.execute(sql, {"table": table})
+    if not result.scalar():
+        print(f"Creating table {table}")
+        sql = text(f"""CREATE TABLE {table}(
+                   id SERIAL PRIMARY KEY, 
+                   author TEXT,
+                   title TEXT,
+                   journal TEXT,
+                   year INT)""")
+        
+        db.session.execute(sql)
+        db.session.commit()
+        print("Table created successfully")
+
+if __name__ == "__main__":
+    with app.app_context():
+      initialize_database()
