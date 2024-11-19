1179103a0ec3ca98eeb0ea2d92f0efc181dbb35b
BorisBanchev <boris.banchev@helsinki.com>
2024-11-19 15:07:14 +0200

    changes to validate_reference function

--- src/util.py
+++ src/util.py
@@ -6,7 +6,7 @@ def validate_reference(author, title, journal, year):
     if len(author) < 3:
         raise UserInputError("Reference author length must be greater than 2")
     
-    if len(title) <= 0 or len(title) > 100:
+    if len(title) < 3 or len(title) > 100:
         raise UserInputError("Reference title length must be smaller than 100 and greater than 2")
     
     if len(journal) < 3:
