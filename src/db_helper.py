bfcc81a1fe1eba4e82970548669daabfff6769de
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2024-11-22 13:31:05 +0200

    Added new fields to table reference_list and new function that delete table if it exists.

--- src/db_helper.py
+++ src/db_helper.py
@@ -10,11 +10,28 @@ def initialize_database():
     if not result.scalar():
         print(f"Creating table {table}")
         sql = text(f"""CREATE TABLE {table}(
-                   id SERIAL PRIMARY KEY, 
+                   id SERIAL PRIMARY KEY,
+                   reference_type TEXT,
+                   keyword TEXT,
                    author TEXT,
                    title TEXT,
+                   booktitle TEXT,
+                   publisher TEXT,
                    journal TEXT,
-                   year INT)""")
+                   year INT,
+                   editor TEXT,
+                   volume TEXT,
+                   number TEXT,
+                   series TEXT,
+                   type TEXT,
+                   chapter TEXT,
+                   pages TEXT,
+                   address TEXT,
+                   edition TEXT,
+                   month TEXT,
+                   note TEXT,
+                   howpublished TEXT,
+                   organization TEXT)""")
         
         db.session.execute(sql)
         db.session.commit()
@@ -26,6 +43,14 @@ def reset_db():
   db.session.execute(sql)
   db.session.commit()
 
+def delete_table():
+   print(f"delete table {table}")
+   sql = text(f"DROP TABLE IF EXISTS {table}")
+   db.session.execute(sql)
+   db.session.commit()
+
+
 if __name__ == "__main__":
     with app.app_context():
+      delete_table()
       initialize_database()
