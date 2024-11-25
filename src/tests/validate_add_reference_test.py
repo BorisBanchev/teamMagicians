9eb3546f136c7bd4a211e4fabf94d096e225275f
BorisBanchev <boris.banchev@helsinki.com>
2024-11-25 19:09:24 +0200

    fixed unittests

--- src/tests/validate_add_reference_test.py
+++ src/tests/validate_add_reference_test.py
@@ -15,7 +15,7 @@ class TestReferenceValidation(unittest.TestCase):
             validate_reference(
                 "article", {"author": "", "title": "test", "journal": "test", "year": "2001", "keyword": "K1235"})
 
-        with self.assertRaises(UserInputError):
+        with self.assertRaises(ValueError):
             validate_reference(
                 "article", {"author": "t", "title": "test", "journal": "test", "year": "2001", "keyword": "K1236"})
 
@@ -24,11 +24,11 @@ class TestReferenceValidation(unittest.TestCase):
                            "author": "test", "title": "test", "journal": "test", "year": "2001", "keyword": "K1237"})
 
     def test_too_long_or_short_title_length_raises_error(self):
-        with self.assertRaises(UserInputError):
+        with self.assertRaises(ValueError):
             validate_reference(
                 "article", {"author": "test", "title": "t", "journal": "test", "year": "2001", "keyword": "K1238"})
 
-        with self.assertRaises(UserInputError):
+        with self.assertRaises(ValueError):
             validate_reference("article", {
                                "author": "test", "title": "test"*100,
                                "journal": "test", "year": "2001", "keyword": "K1239"})
