67f186322a5e4b49199b3e51a11fde371186b9a0
Outi Salonen <147878561+Outisal@users.noreply.github.com>
2024-11-30 15:12:24 +0200

    Add all validate types to util.py and refactor the code

--- src/util.py
+++ src/util.py
@@ -7,11 +7,37 @@ class UserInputError(Exception):
 def validate_reference(reference_type, fields):
     reference_fields = {
         "article": {"mandatory": ["author", "title", "journal", "year"],
-                    "optional": ["volume", "number", "pages", "month", "note"],},
+                    "optional": ["volume", "number", "pages", "month", "note"]},
         "book": {"mandatory": ["author", "title", "publisher", "year"],
                  "optional": ["editor", "volume", "number", "series", "edition", "isbn"]},
         "misc": {"mandatory": [],
                  "optional": ["author", "title", "howpublished", "month", "year", "note"]},
+        "inproceedings": {"mandatory": ["author", "title", "booktitle", "year"],
+                          "optional": ["editor", "volume", "number", "series", "pages",
+                                       "address", "month", "organization", "publisher"]},
+        "booklet": {"mandatory": ["author", "title", "howpublished", "year", "address"],
+                    "optional": ["editor", "volume", "number", "series",
+                                 "organization", "month", "note"]},
+        "conference": {"mandatory": ["author", "title", "booktitle", "year"],
+                       "optional": ["editor", "volume", "number", "series", "pages",
+                                    "address", "month", "organization", "publisher", "note"]},
+        "inbook": {"mandatory": ["author", "title", "booktitle", "publisher", "year"],
+                   "optional": ["editor", "volume", "number", "series", "address", "edition",
+                                "month", "pages", "note"]},
+        "incollection": {"mandatory": ["author", "title", "booktitle", "publisher", "year"],
+                         "optional": ["editor", "volume", "number", "series", "pages", "address", "month"]},
+        "manual": {"mandatory": ["title", "year"],
+                   "optional": ["author", "organization", "address", "edition", "month", "note"]},
+        "mastersthesis": {"mandatory": ["author", "title", "school", "year"],
+                          "optional": ["type", "address", "month", "note"]},
+        "phdthesis": {"mandatory": ["author", "title", "school", "year"],
+                      "optional": ["type", "address", "month", "note"]},
+        "proceedings": {"mandatory": ["title", "year"],
+                        "optional": ["editor", "volume", "number", "series", "address", "month", "publisher"]},
+        "techreport": {"mandatory": ["author", "title", "institution", "year"],
+                       "optional": ["type", "number", "address", "month", "note"]},
+        "unpublished": {"mandatory": ["author", "title", "note"],
+                        "optional": ["month", "year"]}
     }
 
     if not fields["keyword"]:
@@ -36,28 +62,34 @@ def validate_reference(reference_type, fields):
     if not provided_fields:
         raise ValueError("At least one field needs to be maintained.")
 
-    if "author" in fields and check_input_length(fields["author"]):
-        raise ValueError("Reference author length must be greater than 2")
+    rules = {
+        "keyword": (0, 20, "Reference keyword length can't be greater than 20"),
+        "address": (3, 100, "Reference address length must be between 3 and 100"),
+        "author": (3, 100, "Reference author length must be between 3 and 100"),
+        "booktitle": (3, 100, "Reference booktitle length must be between 3 and 100"),
+        "howpublished": (3, 100, "Reference howpublished length must be between 3 and 100"),
+        "institution": (3, 100, "Reference institution length must be between 3 and 100"),
+        "journal": (3, 100, "Reference journal length must be between 3 and 100"),
+        "note": (3, 100, "Reference journal length must be between 3 and 100"),
+        "publisher": (3, 100, "Reference publisher length be between 3 and 100"),
+        "school": (3, 100, "Reference school length must be between 3 and 100"),
+        "title": (3, 100, "Reference title length must be between 3 and 100")
+    }
 
-    if "title" in fields and check_input_length(fields["title"]) or len(fields["title"]) > 100:
-        raise ValueError("Reference title length must be smaller than 100 and greater than 2")
-
-    if "journal" in fields and check_input_length(fields["journal"]):
-        raise ValueError("Reference journal length must be greater than 2")
+    for field, constraint in rules.items():
+        if field in fields:
+            min_length, max_length, error_message = constraint
+            if check_input_length(fields[field], min_length) or len(fields[field]) > max_length:
+                raise ValueError(error_message)
 
     if "year" in fields:  # Check if year exists and is not empty
         if fields["year"] == "":
             pass
-        elif int(fields["year"]) < 0 or int(fields["year"]) > 2025:  # Check range
-            raise ValueError("Reference year must be positive and smaller than 2026")
+        elif int(fields["year"]) < 0 or int(fields["year"]) > 2024:  # Check range
+            raise ValueError("Reference year must be positive and smaller than 2025")
 
-    if "publisher" in fields and check_input_length(fields["publisher"]):
-        raise ValueError("Reference publisher length must be greater than 2")
-
-def check_input_length(field):
-    if len(field) > 0 and len(field) < 3:
-        return True
-    return False
+def check_input_length(field, min_length):
+    return len(field) > 0 and len(field) < min_length
 
 def check_valid_keyword(keyword):
     sql = text("SELECT keyword FROM reference_list")
@@ -65,10 +97,6 @@ def check_valid_keyword(keyword):
     list_keywords = [keyword[0] for keyword in keywords]
     if keyword in list_keywords:
         raise  ValueError("Reference keyword must be unique")
-    if len(keyword) > 20:
-        raise ValueError("Reference keyword length can't be greater than 20")
-    if len(keyword) == 0:
-        raise ValueError("Reference keyword length can't be empty")
 
 def raise_error(message):
     raise ValueError(message)
