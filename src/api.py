281a1d31a4a6939189a9a6b95aaf291a2f5981b9
w1ll1ey <vili.ilari.kaisto@gmail.com>
2024-12-02 15:34:35 +0200

    add optional article fields

--- src/api.py
+++ src/api.py
@@ -11,14 +11,23 @@ mappings = {
 
 def fetch_work(doi):
     work = works.doi(doi)
-    work["author"] = fix_names(work["author"])
-    work["journal"] = work["container-title"][0]
+    if work["author"]:
+        work["author"] = fix_names(work["author"])
+    if work["container-title"][0]:
+        work["journal"] = work["container-title"][0]
     if work["published-print"]:
         work["year"] = work["published"]["date-parts"][0][0]
-    else:
+        work["month"] = work["published"]["date-parts"][0][1]
+    if work["published"]:
         work["year"] = work["published-print"]["date-parts"][0][0]
-    work["title"] = work["title"][0]
-
+        work["month"] = work["published-print"]["date-parts"][0][1]
+    if work["title"][0]:
+        work["title"] = work["title"][0]
+    if work["journal-issue"]["issue"]:
+        work["number"] = work["journal-issue"]["issue"]
+    if work["page"]:    
+        work["pages"] = work["page"]
+        
     return work
 
 def fix_names(authors):
